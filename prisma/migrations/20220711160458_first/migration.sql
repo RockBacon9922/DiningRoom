5d8668e9e99bc55ccbd2d0c34cb7d11a82264905
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-07-14 13:21:27 +0000

    DB schema change. added db files to gitignore

--- prisma/migrations/20220711160458_first/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "locations" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "latitude" REAL NOT NULL,
-    "longitude" REAL NOT NULL
-);
