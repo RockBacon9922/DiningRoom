0bfb770648e6c28be976cd9586e9c7db83ab0625
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-07-25 18:17:06 +0000

    Edited current location api to work off of active value in database

--- pages/api/currentLocation.js
+++ pages/api/currentLocation.js
@@ -1,10 +1,32 @@
-import fs from "fs";
-export default function handler(req, res) {
-	//open file and read first line
-	if (req.query.type !== "post") {
-		var currentLocation = fs.readFileSync("location", "utf8");
-		res.status(200).send(currentLocation);
-		return;
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const handler = async (req, res) => {
+	try {
+		const locations = await getLocations({
+			where: {
+				active: {
+					equals: true,
+				},
+			},
+			select: {
+				id: true,
+			},
+		});
+		const id = JSON.parse(JSON.stringify(locations))[0].id;
+		res.status(200).send(id);
+		console.log("Current Location has and ID of: " + id);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("couldn't find current location");
 	}
-	res.status(200).send("Done!");
-}
+};
+
+export default handler;
+
+const getLocations = async (query) => {
+	const locations = await prisma.locations.findMany(query);
+	await prisma.$disconnect();
+	return locations;
+};
