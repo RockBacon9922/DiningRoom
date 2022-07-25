ab0e78f065886cb2615a33dffde9bb9070964e91
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2022-07-25 12:31:43 +0000

    added prisma disconnect statement. and renamed variable.

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -5,14 +5,14 @@ const prisma = new PrismaClient();
 const handler = async (req, res) => {
 	const query = JSON.parse(JSON.stringify(req.body));
 	console.log("Locations API Called with params: " + JSON.stringify(query));
-	const posts = await getLocations(query);
-	res.status(200).json(posts);
+	const locations = await getLocations(query);
+	res.status(200).json(locations);
 };
 
 export default handler;
 
 const getLocations = async (query) => {
 	const locations = await prisma.locations.findMany(query);
-	locations.forEach((location) => {});
+	await prisma.$disconnect();
 	return locations;
 };
